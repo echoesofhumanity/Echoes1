6c70744a868889026b8415d2c53a0eef0557082f
echoesofhumanity <echoesofhumanity.global@gmail.com>
2026-08-10 16:54:53 +0300

    Create JavaScript foundation

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*
+ * Echoes of Humanity
+ * Main JavaScript Foundation
+ *
+ * This file is intentionally minimal.
+ * Interactive systems will be added in isolated modules.
+ */
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // JavaScript foundation initialized.
+});
