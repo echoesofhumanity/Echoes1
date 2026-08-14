e474398fa3c81a2b17b93b5ce0ea52d13fcff012
echoesofhumanity <echoesofhumanity.global@gmail.com>
2026-08-14 12:14:35 +0300

    Fix hamburger menu functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
-/* ==================================================
-   ECHOES OF HUMANITY
-   MAIN JAVASCRIPT
-   ================================================== */
-
 document.addEventListener("DOMContentLoaded", () => {
 
-  console.log("Echoes of Humanity loaded.");
+    console.log("Echoes of Humanity loaded.");
+
+    const button = document.querySelector(".menu-button");
+    const menu = document.querySelector(".menu-items");
+
+    if (button && menu) {
+
+        button.addEventListener("click", () => {
+            menu.classList.toggle("active");
+        });
+
+    }
 
 });
