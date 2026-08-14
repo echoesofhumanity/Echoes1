eaf90f1744aeccb6378105c10edadf0732659fd7
echoesofhumanity <echoesofhumanity.global@gmail.com>
2026-08-14 12:32:40 +0300

    Fix hamburger menu toggle in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("Echoes of Humanity loaded.");
-
     const button = document.querySelector(".menu-button");
     const menu = document.querySelector(".menu-items");
 
     if (button && menu) {
 
         button.addEventListener("click", () => {
+
             menu.classList.toggle("active");
+
         });
 
     }
